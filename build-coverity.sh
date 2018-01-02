5b51fef185ef47d1af16078c639c31e7e0dc14fc
Dusan Klinec <dusan.klinec@gmail.com>
2018-01-02 23:06:03 +0100

    cov

--- build-coverity.sh
+++ build-coverity.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 eval "${MATRIX_EVAL}"
 bash install-ntl-cached.sh
+ls -las /tmp/coverity-scan-analysis
+ls -las ~/build/ph4r05/Whitebox-crypto-AES
+ls -las /home/travis/build/ph4r05/Whitebox-crypto-AES/cov-int
+
+/tmp/coverity-scan-analysis/cov-configure --comptype gcc --compiler /usr/bin/gcc-4.9
 
 cmake .
 make clean
